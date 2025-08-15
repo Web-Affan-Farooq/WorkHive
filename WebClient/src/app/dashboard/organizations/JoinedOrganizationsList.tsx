f4aa04f1cbdb6ac6c7f3cb82aec01f0fce6c0da5
Web-Affan-Farooq <affanamir903@gmail.com>
2025-08-15 10:48:06 -0700

    fixed null issue in organizations page

--- WebClient/src/app/dashboard/organizations/JoinedOrganizationsList.tsx
+++ WebClient/src/app/dashboard/organizations/JoinedOrganizationsList.tsx
@@ -90,55 +90,52 @@ const JoinedOrganizationsList = () => {
   };
 
   return (
-    <div className="mt-10">
-      <h2 className="font-bold px-2 text-lg">Joined by you</h2>
-      {/* Add joined orgs list here later */}
-      <div className="flex flex-col gap-[10px]">
-        {joinedOrganizations.length <= 0 ? (
-          <p className="text-gray-500 py-20 text-[18px] font-bold text-center">
-            No organizations found ...
-          </p>
-        ) : (
-          joinedOrganizations.map((org, idx) => (
-            <div key={idx} onClick={() => setJoinedOrganization(org.id)}>
-              <ContextMenu>
-                <ContextMenuTrigger>
-                  <Link href={"/organization/joined"}>
-                    <div className="flex flex-row flex-nowrap justify-start items-center gap-[10px] p-5 max-sm:p-2 cursor-pointer">
-                      <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
-                        <Image
-                          src={"/images/building.png"}
-                          alt="image"
-                          width={100}
-                          height={100}
-                          className="object-cover border-3 border-gray-600"
-                        />
-                      </div>
-                      <div>
-                        <h2 className="text-indigo-700 font-bold text-[16px]">
-                          {org.name}
-                        </h2>
-                        <span className="text-sm text-gray-500">
-                          {users[org.id].length} employees
-                        </span>
-                      </div>
+    <div className="flex flex-col gap-[10px]">
+      {joinedOrganizations.length <= 0 ? (
+        <p className="text-gray-500 py-20 text-[18px] font-bold text-center">
+          No organizations found ...
+        </p>
+      ) : (
+        joinedOrganizations.map((org, idx) => (
+          <div key={idx} onClick={() => setJoinedOrganization(org.id)}>
+            <ContextMenu>
+              <ContextMenuTrigger>
+                <Link href={"/organization/joined"}>
+                  <div className="flex flex-row flex-nowrap justify-start items-center gap-[10px] p-5 max-sm:p-2 cursor-pointer">
+                    <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
+                      <Image
+                        src={"/images/building.png"}
+                        alt="image"
+                        width={100}
+                        height={100}
+                        className="object-cover border-3 border-gray-600"
+                      />
                     </div>
-                  </Link>
-                </ContextMenuTrigger>
-                <ContextMenuContent>
-                  <ContextMenuItem>Edit</ContextMenuItem>
-                  <ContextMenuItem
-                    className="text-red-500"
-                    onClick={() => leaveOrganization(org.id)}
-                  >
-                    Delete
-                  </ContextMenuItem>
-                </ContextMenuContent>
-              </ContextMenu>
-            </div>
-          ))
-        )}
-      </div>
+                    <div>
+                      <h2 className="text-indigo-700 font-bold text-[16px]">
+                        {org.name}
+                      </h2>
+                      <span className="text-sm text-gray-500">
+                        {users[org.id].length ? users[org.id].length : 0}
+                        employees
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              </ContextMenuTrigger>
+              <ContextMenuContent>
+                <ContextMenuItem>Edit</ContextMenuItem>
+                <ContextMenuItem
+                  className="text-red-500"
+                  onClick={() => leaveOrganization(org.id)}
+                >
+                  Delete
+                </ContextMenuItem>
+              </ContextMenuContent>
+            </ContextMenu>
+          </div>
+        ))
+      )}
     </div>
   );
 };
